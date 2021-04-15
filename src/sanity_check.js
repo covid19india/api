7653c9ef959fc58ec89d0b14cb8da3f3d9c47d68
harsshjain <65491327+harsshjain@users.noreply.github.com>
2021-04-15 20:39:55 +0530

    Update sanity_check.js

--- src/sanity_check.js
+++ src/sanity_check.js
@@ -2,7 +2,7 @@ const data = require('../tmp/data.json')
 
 var total = data.statewise[0]
 var err = false
-if (total.deltaconfirmed > 200000) {
+if (total.deltaconfirmed > 230000) {
   console.error('Delta confirmed is greater than the limit. Please verify')
   err = true
 }
